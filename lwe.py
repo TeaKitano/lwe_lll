e7005595e96f190b4f3abeeb7870f12192d44314
TeaKitano <chachamusics@outlook.com>
2025-11-05 05:49:11 +0900

    剰余環逆行列実装

--- lwe.py
+++ lwe.py
@@ -3,6 +3,8 @@ from sympy import Matrix, ZZ
 from sympy.matrices.normalforms import hermite_normal_form
 
 from lll import lll
+from utils import inverse
+
 def lwe(a: np.array, b: np.array, q: int):
     # BDD(CVP)に帰着
     n = len(a[0])
@@ -20,15 +22,13 @@ def lwe(a: np.array, b: np.array, q: int):
     B[-1, -1] = 1
     # SVPから最短ベクトルを取得
     e = np.array(lll(B, 0.99)[0][:-1])
-    print(e)
     # エラー項から秘密鍵取得、未完成
     b_d = b[:n]
     e_d = e[:n]
     a_d = a[:n, :]
-    s = np.linalg.solve(a_d, b_d - e_d) % q
-    print(s)
+    s = inverse(a_d, b_d - e_d, q)
 
-    return
+    return e, s
 
 
 if __name__ == "__main__":
@@ -36,11 +36,11 @@ if __name__ == "__main__":
     [4, 13, 24, 7, 16], [20, 9, 22, 27, 8], [19, 8, 19, 3, 1], [18, 22, 4, 8, 18],
     [6, 28, 9, 5, 18], [10, 11, 19, 18, 21], [28, 18, 24, 27, 20]], dtype=int)
     b = np.array([28, 2, 24, 16, 11, 14, 7, 28, 27, 13], dtype=int)
-    lwe(a, b, 29)
-    a = np.array([[14, 15, 5, 2], [13, 14, 14, 6], [6, 10, 13, 1],
-    [10, 4, 12 ,16], [9, 5, 9 , 6], [3, 6, 4, 5], [6, 7, 16, 2]], dtype=int)
-    b = np.array([8, 16, 12, 12, 9, 16, 3], dtype=int)
-    lwe(a, b, 17)
-    a = np.array([[3, 2, 4], [1, 5, -2], [7, 1, 3], [5, 10, -7], [6, 1, 2]], dtype=int)
-    b = np.array([1, 4, 5, 9, 0], dtype=int)
-    lwe(a, b, 17)
+    print(lwe(a, b, 29))
+    # a = np.array([[14, 15, 5, 2], [13, 14, 14, 6], [6, 10, 13, 1],
+    # [10, 4, 12 ,16], [9, 5, 9 , 6], [3, 6, 4, 5], [6, 7, 16, 2]], dtype=int)
+    # b = np.array([8, 16, 12, 12, 9, 16, 3], dtype=int)
+    # lwe(a, b, 17)
+    # a = np.array([[3, 2, 4], [1, 5, -2], [7, 1, 3], [5, 10, -7], [6, 1, 2]], dtype=int)
+    # b = np.array([1, 4, 5, 9, 0], dtype=int)
+    # lwe(a, b, 17)
--- utils.py
+++ utils.py
@@ -36,6 +36,37 @@ def size_reduction(b: np.ndarray):
     
     return b_copy
 
+def inverse(b: np.ndarray, s: np.ndarray,q: int):
+    b = (b + q) % q
+    for i in range(q):
+        pow_i = {j : i ** j % q for j in range(1, q)}
+        if len(set(pow_i.values())) == q-1:
+            break
+    log_i = {j: k for k, j in pow_i.items()}
+    pow_i[0] = 1
+    def mul(x, y):
+        if x == 0 or y == 0:
+            return 0
+        return pow_i[(log_i[x] + log_i[y]) % (q-1)]
+    def div(x, y):
+        if x == 0:
+            return 0
+        return pow_i[(log_i[x] - log_i[y]+q) % (q)]
+    
+    b_i = np.zeros((len(b), len(b)+1), dtype=int)
+    b_i[:, :len(b)] = b
+    for i in range(len(b)):
+        b_i[i][len(b)] = s[i]
+    for i in range(len(b)):
+        b_ii = b_i[i][i]
+        for j in range(len(b)+1):
+            b_i[i][j] = div(b_i[i][j],b_ii)
+        for j in range(len(b)):
+            if i != j:
+                b_i[j] = (b_i[j] - list(map(lambda x: mul(x, b_i[j][i]), b_i[i])) + q) % q
+    return b_i[:, len(b)]
+
 if __name__ == "__main__":
-    b = np.array([[105, 821, 404, 328], [881, 667, 644, 927], [181, 483, 87, 500], [893, 834, 732, 441]])
+    b = np.array([[14,15,5,2],[13,14,14,6],[6,10,13,1],[10,4,12,16]])
     print(size_reduction(b))
+    print(inverse(b, np.array([8,16,12,12]), 17))
